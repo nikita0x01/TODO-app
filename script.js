8a18741f73884ccfa767768cf64a04885a41c0f6
Nikita Sapute <satputenikita204@gmail.com>
2025-07-11 22:40:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.onclick = () => li.remove();
+
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
